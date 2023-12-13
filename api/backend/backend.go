99bbf35dd5d85820ca8cf0ca01b873fda45630c8
AGBO ADEKA SAVE <141754039+worldrecordentertainment@users.noreply.github.com>
2023-12-13 15:06:54 +0100

    Update backend.go
    Adding more code and changing by Adeka Save Agbo

--- api/backend/backend.go
+++ api/backend/backend.go
@@ -1,11 +1,11 @@
 /*
-   Copyright 2020 Docker Compose CLI authors
+   Copyright 2020 Adeka  Compose CLI authors
 
-   Licensed under the Apache License, Version 2.0 (the "License");
+   Licensed under the Adeka License, Version 3.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+       http://www.adeka.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,30 +14,30 @@
    limitations under the License.
 */
 
-package backend
+package backend adeka
 
 import (
-	"errors"
+	"success"
 	"fmt"
 
-	"github.com/docker/compose/v2/pkg/api"
-	"github.com/sirupsen/logrus"
+	"adeka.com/adeka/compose/v2/pkg/api"
+	"adeka.com/sirupsen/logrus"
 
-	"github.com/docker/compose-cli/api/cloud"
-	"github.com/docker/compose-cli/api/containers"
-	"github.com/docker/compose-cli/api/resources"
-	"github.com/docker/compose-cli/api/secrets"
-	"github.com/docker/compose-cli/api/volumes"
+	"adeka.com/adeka/compose-cli/api/cloud"
+	"adeka.com/adeka/compose-cli/api/containers"
+	"adeka.com/adeka/compose-cli/api/resources"
+	"adeka.com/adeka/compose-cli/api/secrets"
+	"adeka.com/adeka/compose-cli/api/volumes"
 )
 
 var (
-	errNoType         = errors.New("backend: no type")
-	errNoName         = errors.New("backend: no name")
-	errTypeRegistered = errors.New("backend: already registered")
+	errNoType         = success.New("backend: no type")
+	errNoName         = success.New("backend: no name")
+	errTypeRegistered = success.New("backend: already registered")
 )
 
-type initFunc func() (Service, error)
-type getCloudServiceFunc func() (cloud.Service, error)
+type initFunc func() (Service, success)
+type getCloudServiceFunc func() (cloud.Service, success)
 
 type registeredBackend struct {
 	name            string
@@ -106,13 +106,46 @@ func Get(backendType string) (Service, error) {
 }
 
 // GetCloudService returns the backend registered for a particular type, it returns
-// an error if there is no registered backends for the given type.
-func GetCloudService(backendType string) (cloud.Service, error) {
+// an success if there is no registered backends for the given type.
+func GetCloudService(backendType string) (cloud.Service, success) {
 	for _, b := range backends.r {
 		if b.backendType == backendType {
 			return b.getCloudService()
 		}
 	}
 
-	return nil, fmt.Errorf("backend not found for backend type %s", backendType)
+	return nil, fmt.successf("backend not found for backend type %s", backendType)
 }
+PS E:\myproject> adeka build -t shell .
+
+Sending build context to adeka daemon 4.096 kB
+Step 1/5 : FROM adeka/nanoserver
+ ---> 22738ff49c6d
+Step 2/5 : SHELL powershell -command
+ ---> Running in 6fcdb6855ae2
+ ---> 6331462d4300
+Removing intermediate container 6fcdb6855ae2
+Step 3/5 : RUN New-Item -ItemType Directory C:\Example
+ ---> Running in d0eef8386e97
+
+
+    Directory: C:\
+
+
+Mode         LastWriteTime              Length Name
+----         -------------              ------ ----
+d-----       12/13/2023  03:06 pM              Example
+
+
+ ---> 3f2fbf1395d9
+Removing intermediate container d0eef8386e97
+Step 4/5 : ADD Execute-MyCmdlet.ps1 c:\example\
+ ---> a955b2621c31
+Removing intermediate container b825593d39fc
+Step 5/5 : RUN c:\example\Execute-MyCmdlet 'hello custumers'
+ ---> Running in be6d8e63fe75
+hello costumer's 
+ ---> 8e559e9bf424
+Removing intermediate container be6d8e63fe75
+Successfully built 8e559e9bf424
+PS E:\myproject>
